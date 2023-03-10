fb582868adfd9b93c31e1d43033a1e0689a029c9
Raghav Gupta <ragaws1674@gmail.com>
2023-03-10 21:39:28 +0530

    Add counter endpoint

--- main.go
+++ main.go
@@ -12,14 +12,22 @@ const (
 	PORT = 8000
 )
 
+var (
+	counter int 
+)
+
 func main(){
 
-	
 	r := http.NewServeMux()
 
 	r.HandleFunc("/",func(w http.ResponseWriter, req *http.Request){
 		fmt.Fprintf(w,"Hello World!!")
 		log.Printf("%s %d %s",req.Method,200,req.URL.Path)
+	})
+
+	r.HandleFunc("/counter",func(w http.ResponseWriter, req *http.Request){
+		fmt.Fprintf(w,"This page is visited %d times",counter)
+		counter++
 	})
 
 	log.Printf("Server is listening on port %d",PORT)
