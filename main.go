588cd49ced517d7b5161d073711d9d7f5c7b4329
Raghav Gupta <ragaws1674@gmail.com>
2023-03-10 21:58:27 +0530

    Update counter to /view

--- main.go
+++ main.go
@@ -25,10 +25,11 @@ func main(){
 		log.Printf("%s %d %s",req.Method,200,req.URL.Path)
 	})
 
-	r.HandleFunc("/counter",func(w http.ResponseWriter, req *http.Request){
+	r.HandleFunc("/views",func(w http.ResponseWriter, req *http.Request){
 		fmt.Fprintf(w,"This page is visited %d times",counter)
 		counter++
 	})
+
 
 	log.Printf("Server is listening on port %d",PORT)
 	http.ListenAndServe(fmt.Sprintf(":%d",PORT),r)
